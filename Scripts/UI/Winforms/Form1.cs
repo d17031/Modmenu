fb4c6209c7a6e674a8a58c6b71a7ebf78cd6368d
ShadowDev <teammicro2026@outlook.com>
2026-03-13 11:42:34 +0100

    Update Form1.cs

--- Scripts/UI/Winforms/Form1.cs
+++ Scripts/UI/Winforms/Form1.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Windows.Forms;
 
+namespace WinFormsTemplate
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
