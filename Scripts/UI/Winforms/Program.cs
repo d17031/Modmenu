17673f6def22be3bbf722e8aaab83a64fc24f7a8
ShadowDev <teammicro2026@outlook.com>
2026-03-13 11:37:20 +0100

    Update Program.cs

--- Scripts/UI/Winforms/Program.cs
+++ Scripts/UI/Winforms/Program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Windows.Forms;
 
+namespace WinFormsTemplate
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
